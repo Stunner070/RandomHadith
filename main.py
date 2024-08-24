eb4090b98778c0287b411f6d4f0e769255e74731
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2024-08-24 21:10:56 +0200

    Basic functionality of finding random hadith is done + basic frontend

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
+import random as random
+import csv as csv
+import sys
+
+from PyQt5 import QtWidgets, uic
+
+
 def main():
-    print("Hello, World!")
-    # random()
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+def randomH():
+    number_of_hadith = random.randint(1, 2006)
+    with open('all_hadiths_clean.csv') as hadiths:
+        one_hadith = csv.DictReader(hadiths)
+        for row in one_hadith:
+            if int(row['hadith_no']) == (number_of_hadith):
+                return row['chapter'] + row['text_en']
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi('main_window.ui', self)
+
+        # Connect button to function
+        self.randomButton.clicked.connect(self.display_random_hadith)
+
+    def display_random_hadith(self):
+        hadith = randomH()
+        self.hadithText.setPlainText(hadith)
 
 if __name__ == "__main__":
     main()
-
-# def random():
